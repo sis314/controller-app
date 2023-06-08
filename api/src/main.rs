f0dfebc68fac58b011f59fcc64274d47b81df202
yum <@numazu.kosen-ac.jp>
2023-06-08 21:04:27 +0900

    [feature]change sending data structure

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,6 +2,7 @@ use once_cell::sync::Lazy; // 1.3.1
 use serial::Serial;
 use std::sync::Mutex;
 use std::time::Duration;
+use std::num::Wrapping;
 
 mod error;
 mod serial;
@@ -14,7 +15,7 @@ fn main() {
         Ok(_) => (),
     }
     loop {
-        match send(1, 2, 3, 4, 5, 6) {
+        match send(1, 2, 3, 4, 5) {
             Err(e) => println!("{}", e),
             Ok(_) => (),
         };
@@ -33,10 +34,10 @@ fn set_port(path: &str) -> Result<(), String> {
     }
 }
 
-fn send(address: u8, mn: u8, func: u8, dir: u8, speed: u8, id: u8) -> Result<(), String> {
+fn send(address: u8, mn: u8,dir: u8, speed: u8, id: u8) -> Result<(), String> {
     let mut serial = SERIAL.lock().unwrap();
     let send_num: u8 = 0; //*todo */
-    let data: Vec<u8> = format(send_num, address, mn, func, dir, speed, id);
+    let data: Vec<u8> = format(send_num, address, mn, dir, speed);
 
     match serial.send(data, send_num) {
         Ok(_) => Ok(()),
@@ -47,16 +48,10 @@ fn send(address: u8, mn: u8, func: u8, dir: u8, speed: u8, id: u8) -> Result<(),
     }
 }
 
-fn format(send_num: u8, address: u8, mn: u8, func: u8, dir: u8, speed: u8, id: u8) -> Vec<u8> {
+fn format(send_num: u8, address: u8, mn: u8, dir: u8, speed: u8) -> Vec<u8> {
     let header: u8 = 0x40;
     let footer: u8 = 0x00;
-    let length: u8 = 4;
-    let data3 = (mn << 4) | (func << 1) | 1;
-
-    let sum: u16 = (send_num + address + data3 + speed).into();
-    let check_sum: u8 = sum as u8;
-
-    vec![
-        header, length, send_num, address, data3, speed, check_sum, footer,
-    ]
+    let data: u8 = ((mn<<6)&0b11000000) + (dir&1);
+    let sum: u8 = (Wrapping(send_num) + Wrapping(address) + Wrapping(data) + Wrapping(speed)).0;
+    vec![header,send_num,address,data,speed,sum,footer]
 }
